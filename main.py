04223c974c65fe25bdab202f35212a0dfe54fc65
almeidava93 <88211872+almeidava93@users.noreply.github.com>
2022-09-08 19:44:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -51,6 +51,12 @@ with column_c:
     st.subheader("Artigos excluídos")
     st.markdown(f"{len(dashboard_data[dashboard_data['excluded']==True])}")
 
+st.markdown("""
+
+
+
+""")
+
 st.markdown("**Status para avaliação de título e abstract de cada revisor**")
 
 def get_user_review_progress(user):
