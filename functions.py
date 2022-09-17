8a2f55a919e13fe0891b1508ae3604c5d78b7762
almeidava93 <88211872+almeidava93@users.noreply.github.com>
2022-09-17 10:33:59 -0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -85,8 +85,7 @@ def add_new_articles(file, search_strategy):
     #Load current database articles
     query = firestore_client.collection("articles_simplified").get()
     data = [doc.to_dict() for doc in query]
-    df1 = pd.DataFrame.from_records(data)
-    #[['pubmed_id']]
+    df1 = pd.DataFrame.from_records(data)[['pubmed_id']]
     #.astype('int32')
     #.sort_values(by=['pubmed_id'], ascending=True).reset_index()
 
@@ -99,8 +98,7 @@ def add_new_articles(file, search_strategy):
     string_data = stringio.read()
     
     articles_data = nbib.read(string_data)
-    df2 = pd.DataFrame.from_records(articles_data)
-    #[['pubmed_id']]
+    df2 = pd.DataFrame.from_records(articles_data)[['pubmed_id']]
     #.astype('int32').sort_values(by=['pubmed_id'], ascending=True).reset_index()
 
     st.write(df1)
