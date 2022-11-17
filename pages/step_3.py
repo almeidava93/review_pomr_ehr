a81942cef62937d5ae3131c3d473fcb29651033f
almeidava93 <almeida.va93@gmail.com>
2022-11-17 16:08:55 -0300

    Update step_3.py

--- pages/step_3.py
+++ pages/step_3.py
@@ -23,104 +23,99 @@ import functions
 reviewed_articles_df = functions.retrieve_fully_reviewed_articles()
 
 #2) Update database with articles that were included or excluded by unanimity
-reviewed_articles_df['excluded'] = reviewed_articles_df['almeida.va93@gmail.com_excluded'] + reviewed_articles_df['henrique.t.arai@gmail.com_excluded'] + reviewed_articles_df['mariela204@gmail.com_excluded']
-reviewed_articles_df['included'] = reviewed_articles_df['almeida.va93@gmail.com_included'] + reviewed_articles_df['henrique.t.arai@gmail.com_included'] + reviewed_articles_df['mariela204@gmail.com_included']
+@st.cache(ttl = 1800)
+def update_database_1():
+    reviewed_articles_df['excluded'] = reviewed_articles_df['almeida.va93@gmail.com_excluded'] + reviewed_articles_df['henrique.t.arai@gmail.com_excluded'] + reviewed_articles_df['mariela204@gmail.com_excluded']
+    reviewed_articles_df['included'] = reviewed_articles_df['almeida.va93@gmail.com_included'] + reviewed_articles_df['henrique.t.arai@gmail.com_included'] + reviewed_articles_df['mariela204@gmail.com_included']
 
-included_articles = reviewed_articles_df['included'][reviewed_articles_df['included']==3]
-excluded_articles = reviewed_articles_df['excluded'][reviewed_articles_df['excluded']==3]
-undefined_articles = reviewed_articles_df[(reviewed_articles_df['excluded']!=3) & (reviewed_articles_df['included']!=3)]
+    included_articles = reviewed_articles_df['included'][reviewed_articles_df['included']==3]
+    excluded_articles = reviewed_articles_df['excluded'][reviewed_articles_df['excluded']==3]
+    undefined_articles = reviewed_articles_df[(reviewed_articles_df['excluded']!=3) & (reviewed_articles_df['included']!=3)]
+
+    return included_articles, excluded_articles, undefined_articles
+
+included_articles, excluded_articles, undefined_articles = update_database_1()
 
 #3) Set up a new collection of articles for step two. They will have the following structure: pubmed_id, included, excluded. Retrieve 
 #the articles that are already in that collection
-articles_df = functions.get_dashboard_data_step_two()
+@st.cache(ttl = 1800)
+def update_database_2():
+    articles_df = functions.get_dashboard_data_step_two()
 
-if len(articles_df) > 0:
-    #A) add to the collection the included articles
-    for article_id in tqdm(included_articles.index):
-        if article_id not in articles_df["pubmed_id"].values:
-            doc_ref = functions.firestore_client.collection("articles_second_review").document(f"{article_id}")
-            doc_ref.set({
-                "pubmed_id": article_id,
-                "included": True,
-                "excluded": False        
-            })
+    if len(articles_df) > 0:
+        #A) add to the collection the included articles
+        for article_id in tqdm(included_articles.index):
+            if article_id not in articles_df["pubmed_id"].values:
+                doc_ref = functions.firestore_client.collection("articles_second_review").document(f"{article_id}")
+                doc_ref.set({
+                    "pubmed_id": article_id,
+                    "included": True,
+                    "excluded": False        
+                })
 
-    #B) add to the collection the excluded articles
-    for article_id in tqdm(excluded_articles.index):
-        if article_id not in articles_df["pubmed_id"].values:
-            doc_ref = functions.firestore_client.collection("articles_second_review").document(f"{article_id}")
-            doc_ref.set({
-                "pubmed_id": article_id,
-                "included": False,
-                "excluded": True        
-            })
+        #B) add to the collection the excluded articles
+        for article_id in tqdm(excluded_articles.index):
+            if article_id not in articles_df["pubmed_id"].values:
+                doc_ref = functions.firestore_client.collection("articles_second_review").document(f"{article_id}")
+                doc_ref.set({
+                    "pubmed_id": article_id,
+                    "included": False,
+                    "excluded": True        
+                })
 
-    #C) add to the collection the undefined articles
-    for article_id in tqdm(undefined_articles.index):
-        if article_id not in articles_df["pubmed_id"].values:
-            doc_ref = functions.firestore_client.collection("articles_second_review").document(f"{article_id}")
-            doc_ref.set({
-                "pubmed_id": article_id,
-                "included": False,
-                "excluded": False        
-            })
+        #C) add to the collection the undefined articles
+        for article_id in tqdm(undefined_articles.index):
+            if article_id not in articles_df["pubmed_id"].values:
+                doc_ref = functions.firestore_client.collection("articles_second_review").document(f"{article_id}")
+                doc_ref.set({
+                    "pubmed_id": article_id,
+                    "included": False,
+                    "excluded": False        
+                })
 
-else:
-    #A) add to the collection the included articles
-    for article_id in tqdm(included_articles.index):
-            doc_ref = functions.firestore_client.collection("articles_second_review").document(f"{article_id}")
-            doc_ref.set({
-                "pubmed_id": article_id,
-                "included": True,
-                "excluded": False        
-            })
+    else:
+        #A) add to the collection the included articles
+        for article_id in tqdm(included_articles.index):
+                doc_ref = functions.firestore_client.collection("articles_second_review").document(f"{article_id}")
+                doc_ref.set({
+                    "pubmed_id": article_id,
+                    "included": True,
+                    "excluded": False        
+                })
 
-    #B) add to the collection the excluded articles
-    for article_id in tqdm(excluded_articles.index):
-            doc_ref = functions.firestore_client.collection("articles_second_review").document(f"{article_id}")
-            doc_ref.set({
-                "pubmed_id": article_id,
-                "included": False,
-                "excluded": True        
-            })
+        #B) add to the collection the excluded articles
+        for article_id in tqdm(excluded_articles.index):
+                doc_ref = functions.firestore_client.collection("articles_second_review").document(f"{article_id}")
+                doc_ref.set({
+                    "pubmed_id": article_id,
+                    "included": False,
+                    "excluded": True        
+                })
 
-    #C) add to the collection the undefined articles
-    for article_id in tqdm(undefined_articles.index):
-            doc_ref = functions.firestore_client.collection("articles_second_review").document(f"{article_id}")
-            doc_ref.set({
-                "pubmed_id": article_id,
-                "included": False,
-                "excluded": False   
-            })
+        #C) add to the collection the undefined articles
+        for article_id in tqdm(undefined_articles.index):
+                doc_ref = functions.firestore_client.collection("articles_second_review").document(f"{article_id}")
+                doc_ref.set({
+                    "pubmed_id": article_id,
+                    "included": False,
+                    "excluded": False   
+                })
+
+update_database_2()
 
 #4) Get all included articles from articles_second_review, check if it is already present in articles_third_review
 #   and, if not, add to articles_third_review to be reevaluated
-query_ref_2 = functions.firestore_client.collection("articles_second_review").where("included", "==", True)
-query = query_ref_2.get()
-data = [doc.to_dict() for doc in query]
-df = pd.DataFrame.from_records(data)
-
-if len(functions.firestore_client.collection("articles_third_review").get()) == 0:
-    print("Including all articles...")
-    doc_ref = functions.firestore_client.collection("articles_third_review")
-    for article_id in tqdm(df["pubmed_id"]):
-        doc_ref.document(f"{article_id}").set({
-            "pubmed_id": article_id,
-            "included": False,
-            "excluded": False,
-            "article_data": "",
-            "reviewer": random.choice(functions.reviewers),
-            "concluded": False
-        })
-
-elif len(functions.firestore_client.collection("articles_third_review").get()) > 0:
-    print("Checking for any new articles to include...")
-    doc_ref = functions.firestore_client.collection("articles_third_review")
-    query = doc_ref.get()
+@st.cache(ttl = 1800)
+def update_database_3():
+    query_ref_2 = functions.firestore_client.collection("articles_second_review").where("included", "==", True)
+    query = query_ref_2.get()
     data = [doc.to_dict() for doc in query]
-    df1 = pd.DataFrame.from_records(data)
-    for article_id in tqdm(df["pubmed_id"]):
-        if article_id not in list(df1["pubmed_id"]):
+    df = pd.DataFrame.from_records(data)
+
+    if len(functions.firestore_client.collection("articles_third_review").get()) == 0:
+        print("Including all articles...")
+        doc_ref = functions.firestore_client.collection("articles_third_review")
+        for article_id in tqdm(df["pubmed_id"]):
             doc_ref.document(f"{article_id}").set({
                 "pubmed_id": article_id,
                 "included": False,
@@ -129,9 +124,28 @@ elif len(functions.firestore_client.collection("articles_third_review").get()) >
                 "reviewer": random.choice(functions.reviewers),
                 "concluded": False
             })
-            print(f"article with pubmed_id {article_id} modified")
 
+    elif len(functions.firestore_client.collection("articles_third_review").get()) > 0:
+        print("Checking for any new articles to include...")
+        doc_ref = functions.firestore_client.collection("articles_third_review")
+        query = doc_ref.get()
+        data = [doc.to_dict() for doc in query]
+        df1 = pd.DataFrame.from_records(data)
+        for article_id in tqdm(df["pubmed_id"]):
+            if article_id not in list(df1["pubmed_id"]):
+                doc_ref.document(f"{article_id}").set({
+                    "pubmed_id": article_id,
+                    "included": False,
+                    "excluded": False,
+                    "article_data": "",
+                    "reviewer": random.choice(functions.reviewers),
+                    "concluded": False
+                })
+                print(f"article with pubmed_id {article_id} modified")
+    
+    return df1
 
+df1 = update_database_3()
 
 st.title("Passo 3: Lendo artigos completos")
 st.markdown("""***""")
@@ -185,6 +199,7 @@ included = st.radio("Incluir ou excluir?", [True, False], index=0, format_func=f
 
 if included:
     article_review_data["included"] = True
+    article_review_data["excluded"] = False
     tags = st.multiselect("Quais as características deste estudo? Selecione todas as que se aplicam",
         options=["estudo epidemiologico",
             "lista de problemas",
@@ -204,6 +219,7 @@ if included:
 
 elif included==False:
     article_review_data["included"] = False
+    article_review_data["excluded"] = True
 
 
 st.markdown("***")
